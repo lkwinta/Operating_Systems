7278a330836074d93286777dfff8835848be1ce0
Łuaksz Kwinta <lukaszmind@gmail.com>
2024-02-26 23:40:18 +0100

    example lab

--- lab1/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    system("wc t.txt > /tmp/temp_wc.txt");
-    printf("Hello World\n");
-}
---
+++ zad01/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "libwc/libwc.h"
+
+int main() {
+    printf("Hello World\n");
+    LibWCMemory mem = LibWCMemory_create(1600);
+
+    printf("Hello World\n");
+}
