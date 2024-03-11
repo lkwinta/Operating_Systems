abee5e52e72953d8fc717989ee677b5561ac753b
Łuaksz Kwinta <lukaszmind@gmail.com>
2024-03-11 17:23:13 +0100

    fix define macro

--- lab02/client.c
+++ lab02/client.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-#include <dlfcn.h>
+
+#ifndef DYNAMIC_DLOPEN
+    #include "collatzlib/collatz.h"
+#endif //DYNAMIC_DLOPEN
+
+#ifdef DYNAMIC_DLOPEN
+#include "dlfcn.h"
+
+int (*collatz_conjecture)(int input);
+int (*test_collatz_convergence)(int input, int max_iter);
+#endif //DYNAMIC_DLOPEN
 
 int main() {
-    int (*collatz_conjecture)(int input);
-    int (*test_collatz_convergence)(int input, int max_iter);
+#ifdef DYNAMIC_DLOPEN
+    
 
     void* dll_handle = dlopen("collatzlib/build/libcollatz_shared.so", RTLD_LAZY);
     if (!dll_handle) {
@@ -13,6 +23,7 @@ int main() {
 
     collatz_conjecture = dlsym(dll_handle, "collatz_conjecture");
     test_collatz_convergence = dlsym(dll_handle, "test_collatz_convergence");
+#endif //DYNAMIC_DLOPEN
 
     printf("%d\n", collatz_conjecture(10));
     printf("%d\n", test_collatz_convergence(10, 100));
--- lab02/client_static.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "collatzlib/collatz.h"
-
-int main() {
-    printf("%d\n", collatz_conjecture(10));
-    printf("%d\n", test_collatz_convergence(10, 100));
-}
