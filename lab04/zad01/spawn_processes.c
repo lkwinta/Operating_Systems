2b9430621ff6beebb9dd4d67866f3f90deb7d899
lkwinta <lukaszmind@gmail.com>
2024-03-19 16:19:54 +0100

    lab4 done

--- lab04/zad01/spawn_processes.c
+++ lab04/zad01/spawn_processes.c
@@ -1,6 +1,34 @@
 #include <sys/resource.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main() {
+int main(int argc, char** argv) {
+     // if we didn't get 2 arguments we cannot procceed
+    if(argc < 2) {
+        printf("Usage: %s <number_of_processes>\n", argv[0]);
+        return -1;
+    }
 
+    long number_of_processes = strtol(argv[1], NULL, 10);
+    if(number_of_processes < 0){
+        printf("Number of processes must be a positive number\n");
+        return -1;
+    }
+
+    for(int i = 0; i < number_of_processes; i++) {
+        pid_t pid = fork();
+        if(pid == 0) {
+            printf("Child process pid %d\n", getpid());
+            printf("Parent process pid %d\n", getppid());
+            exit(0);
+        }
+    }
+    
+    while(wait(NULL) > 0);
+    printf("Number of processes: %ld\n", number_of_processes);
+    
     return 0;
 }
--- lab04/zad02/execute.c
+++ lab04/zad02/execute.c
@@ -1,6 +1,56 @@
-#include <sys/resource.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
-int main() {
+// declare global variable
+int global = 0;
 
-    return 0;
+/**
+ * argv[0] - execution command - always passed as default
+ * argv[1] - directory path for ls command
+*/
+int main(int argc, char** argv) {
+     // if we didn't get 3 arguments we cannot procceed
+    if(argc < 2) {
+        printf("Usage: %s <path to directory>\n", argv[0]);
+        return -1;
+    }
+
+    /* print program name */
+    printf("%s \n", strrchr(argv[0], '/') + 1);
+
+    /* declar local variable */
+    int local = 0;
+
+    /* spawn child process for ls execution */
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        /* Execute childs code */
+        printf("child process\n");
+        global++;
+        local++;
+
+        printf("child pid = %d, parent pid = %d \n", getpid(), getppid());
+        printf("child's local = %d, child's global = %d \n", local, global);
+    
+        int status = execl("/bin/ls", "ls", "-l", argv[1], NULL);
+
+        /* exit from child with ls return code */
+        exit(status);
+    } 
+
+    int status = 0;
+    wait(&status);
+    int child_return_status = WEXITSTATUS(status);
+
+    printf("parent process\n");
+    printf("parent pid = %d, child pid = %d \n", getpid(), pid);
+    printf("Child exit code: %d \n", child_return_status);
+    printf("parent's local = %d, parent's global = %d \n", local, global);
+
+    return child_return_status;
 }
