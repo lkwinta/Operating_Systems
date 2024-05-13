2fc452c7efff601318a2ee70a923040539b24c69
Łuaksz Kwinta <lukaszmind@gmail.com>
2024-05-13 22:22:36 +0200

    lab10 comments

--- lab10/santa.c
+++ lab10/santa.c
@@ -3,10 +3,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * Declare global variables for easy access between threads
+*/
+
+/* mutex and condition variable to wake up santa */
 pthread_mutex_t santa_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t santa_cond = PTHREAD_COND_INITIALIZER;
 
-pthread_mutex_t renifer_mutex[9] = {
+/* declare mutexes for all reindeers */
+pthread_mutex_t reindeers_mutexes[9] = {
     PTHREAD_MUTEX_INITIALIZER,
     PTHREAD_MUTEX_INITIALIZER,
     PTHREAD_MUTEX_INITIALIZER,
@@ -18,29 +24,53 @@ pthread_mutex_t renifer_mutex[9] = {
     PTHREAD_MUTEX_INITIALIZER
 };
 
-int renifers_back_count = 0;
-pthread_mutex_t renifers_back_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_t santa, renifers[9];
+/* declare mutex and counter to count reindeers that has come back */
+int reindeers_back_count = 0;
+pthread_mutex_t reindeers_back_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void* renifer_thread(void* arg){
-    int id = *(int*)arg;
+/* declare thread handles for santa and reindeer threads*/
+pthread_t santa_thread;
+pthread_t reindeers_threads[9];
+
+/**
+ * @brief Function that is executed by reindeer threads
+ * 
+ * @param arg - id of the reindeer
+ * 
+ * @return NULL
+*/
+void* reindeer_thread(void* arg){
+    // cast argument to int
+    int id = (int)arg;
+    // set cancel type to asynchronous to be able to cancel thread immediately
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
 
-    pthread_mutex_lock(&renifer_mutex[id]);
+    // lock reindeer mutex to prevent cycling until reindeers do not come back from santa claus - when santa releases their mutexes
+    pthread_mutex_lock(&reindeers_mutexes[id]);
     while(1) {
+        // reindeer is on vacation for 5-10 seconds
         sleep(rand() % 6 + 5);
 
-        pthread_mutex_lock(&renifers_back_mutex);
-        printf("Renifer: czeka %d reniferów, %d\n", renifers_back_count, id);
-        renifers_back_count++;
-        if (renifers_back_count == 9) {
-            pthread_cond_signal(&santa_cond);
-            printf("Renifer: budzę Mikołaja, %d\n", id);
-            renifers_back_count = 0;
-        }
-        pthread_mutex_unlock(&renifers_back_mutex);
+        // lock counter mutex to increment counter of reindeers that has come back
+        pthread_mutex_lock(&reindeers_back_mutex);
+        printf("Renifer: czeka %d reniferów, %d\n", reindeers_back_count, id);
 
-        pthread_mutex_lock(&renifer_mutex[id]);
+        // increment counter and check if all reindeers are back
+        reindeers_back_count++;
+        if (reindeers_back_count == 9) {
+            printf("Renifer: budzę Mikołaja, %d\n", id);
+            // if all reindeers are back, wake up santa and reset counter
+            pthread_cond_signal(&santa_cond);
+            reindeers_back_count = 0;
+        }
+
+        // unlock counter mutex
+        pthread_mutex_unlock(&reindeers_back_mutex);
+
+        // wait for santa to release reindeer mutex
+        pthread_mutex_lock(&reindeers_mutexes[id]);
+
+        // reindeer is flying on vacation
         printf("Renifer: lecę na wakacje, %d\n", id);
     }  
 
@@ -48,39 +78,45 @@ void* renifer_thread(void* arg){
 }
 
 void* santa_thread(void* arg) {
+    // cycle 4 times to deliver toys
     for (int i = 0; i < 4; i++) {
+        // wait for reindeers to wake up santa
         pthread_cond_wait(&santa_cond, &santa_mutex);
         printf("Mikołaj: budzę się\n");
 
         printf("Mikołaj: dostarczam zabawki\n");
+        // santa is delivering toys for 2-4 seconds
         sleep(rand() % 3 + 2);
+
+        // release all reindeers
         for (int j = 0; j < 9; j++) {
-            pthread_mutex_unlock(&renifer_mutex[j]);
+            pthread_mutex_unlock(&reindeers_mutexes[j]);
         }
 
         printf("Mikołaj: zasypiam\n");
     }
 
+    // cancel all reindeer threads
     for (int j = 0; j < 9; j++) {
-        pthread_cancel(renifers[j]);
+        pthread_cancel(reindeers_threads[j]);
     }
 
     return NULL;
 }
 
 int main() {
-    int ids[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-
-    pthread_create(&santa, NULL, santa_thread, NULL);
+    // create santa and reindeer threads
+    pthread_create(&santa_thread, NULL, santa_thread, NULL);
     for (int i = 0; i < 9; i++) {
-        pthread_create(&renifers[i], NULL, renifer_thread, &ids[i]);
+        pthread_create(&reindeers_threads[i], NULL, reindeer_thread, (void*)i);
     }
 
-    pthread_join(santa, NULL);
+    // wait for santa and reindeer threads to finish
+    pthread_join(santa_thread, NULL);
     for (int i = 0; i < 9; i++) {
-        pthread_join(renifers[i], NULL);
+        pthread_join(reindeers_threads[i], NULL);
     }
-
+    
     printf("Koniec\n");
 
     return 0;
