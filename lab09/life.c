7ae481eaac8b3a03cbabf8c8875befe5754a21fd
Łuaksz Kwinta <lukaszmind@gmail.com>
2024-05-13 18:57:51 +0200

    lab09 comments

--- lab09/life.c
+++ lab09/life.c
@@ -1,3 +1,4 @@
+// declare feature test macros to enable certain functions (sigaction, usleep)
 #define _XOPEN_SOURCE 700
 #define _DEFAULT_SOURCE
 
@@ -13,27 +14,39 @@
 #include <math.h>
 #include <signal.h>
 
-#define THREAD_COUNT 12
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define THREAD_COUNT 12 // define number of threads to use - don't exceed number of threads on your CPU
 
+/**
+ * Define structure that will be passed to the thread function 
+ * as an argument
+*/
 typedef struct {
-	int cell_start;
-	int cell_end;
+	int cell_start; // index of the starting cell (inclusive)
+	int cell_end; // index of the ending cell (exclusive)
 
-	char** background;
-	char** foreground;
+	char** background; // pointer to the background array
+	char** foreground; // pointer to the foreground array
 } thread_args_t;
 
+/* declare dummy signal handler so the signal doesn't crash program */
 void dummy_handler(int signo) {__asm__("nop");}
 
 void* thread_function(void* arg) {
+	/* dereference arguments struct */
 	thread_args_t* args = (thread_args_t*)arg;
 	
+	/* thread loop */
 	while (true) {
+		/* pause thread until any signal comes */
 		pause();
 
+		/* iterate over assigned cel */
 		for (int i = args->cell_start; i < args->cell_end; i++) {
-			int row = i / GRID_WIDTH;
-			int col = i % GRID_WIDTH;
+			int row = i / GRID_WIDTH; // translate index to row
+			int col = i % GRID_WIDTH; // translate index to column
+
+			/* update cell in background array */
 			(*args->background)[i] = is_alive(row, col, *args->foreground);
 		}
 	}
@@ -41,15 +54,17 @@ void* thread_function(void* arg) {
 
 int main()
 {
+	/**
+	 * Set up signal handler for threads
+	 * We need to set up a dummy signal handler so the signal doesn't crash the program
+	 * We will use the signal to wake up the threads
+	*/
 	struct sigaction sa;
 	sa.sa_handler = dummy_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
 	sigaction(SIGUSR1, &sa, NULL);
 
-	pthread_t threads[THREAD_COUNT];
-	thread_args_t args[THREAD_COUNT];
-	
 	srand(time(NULL));
 	setlocale(LC_CTYPE, "");
 	initscr(); // Start curses mode
@@ -58,13 +73,23 @@ int main()
 	char *background = create_grid();
 	char *tmp;
 
+	pthread_t threads[THREAD_COUNT];
+	thread_args_t args[THREAD_COUNT];
+
 	int cells_per_thread = (int)ceil(GRID_HEIGHT * GRID_WIDTH / THREAD_COUNT);
 	for (int i = 0; i < THREAD_COUNT; i++) {
+		// Assign cells to threads
 		args[i].cell_start = i * cells_per_thread;
-		args[i].cell_end = (i + 1) * cells_per_thread;
+		args[i].cell_end = MIN((i + 1) * cells_per_thread, GRID_HEIGHT * GRID_WIDTH); // exclusive, safety check for the last thread
+
+		/**
+		 * We use pointers to the 1D array to be able to shuffle pointers between frames
+		 * instead of copying the whole array
+		*/
 		args[i].foreground = &foreground;
 		args[i].background = &background;
 
+		// create threads without any special attributes and with argument structure
 		pthread_create(&threads[i], NULL, thread_function, &args[i]);
 	}
 
@@ -73,6 +98,8 @@ int main()
 	while (true)
 	{
 		draw_grid(foreground);
+
+		// Wake up threads with declared signals
 		for(int i = 0; i < THREAD_COUNT; i++) {
 			pthread_kill(threads[i], SIGUSR1);
 		}
